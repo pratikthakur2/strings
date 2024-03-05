46af588854c1058b2f095bde5c62aa51786fa415
Pratik Thakur <prateekthakur557@gmail.com>
2024-03-05 15:16:31 +0530

    Strings using CPP
    String programs using CPP

---
+++ stringinit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str1;
+    string str2 = "Hello nigga\n";
+
+    string str3("welcum"); //another way to initialize 
+    cout<<str2;
+    cout<<str3;
+
+
+}
